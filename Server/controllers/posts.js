01b2c9ae5718a565fbf9d4d54304b476fa3b1c5b
Shuming Zhang <chriszhangshu@gmail.com>
2023-01-30 19:58:40 -0500

    Add post controller for get/add/remove/like on posts

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -27,18 +27,50 @@ export const createPost = async (req, res) => {
 }
 
 // read
+// get news post from everyone
 export const getFeedPosts = async (req, res) => {
     try {
-        
+        const post = await Post.find();
+        res.status(201).json(post);
     }
     catch (err){
         res.status(404).json({message: err.message});
     }
 }
 
+// get posts from a specific user
 export const getUserPosts = async (req, res) => {
     try{
+        const { userId }= req.params;
+        const post = await Post.find({ userId });
+        res.status(201).json(post);
+    }
+    catch (err){
+        res.status(404).json({message: err.message});
+    }
+}
 
+// update
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if (isLiked){
+            post.likes.delete(userId);
+        }
+        else{
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            {likes: post.likes},
+            {new:true}
+        );
+        res.status(200).json();
     }
     catch (err){
         res.status(404).json({message: err.message});
